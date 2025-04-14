8e40f2fa1995691f1c72a658b5748131ea12b4fc
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2025-04-14 18:25:17 +0200

    feat(nvim): add more plugins and setup lsp

--- nvim/.config/nvim/lua/adam/lazy.lua
+++ nvim/.config/nvim/lua/adam/lazy.lua
@@ -14,7 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("adam.plugins", {
+require("lazy").setup({ { import = "adam.plugins" }, { import = "adam.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = true,
--- nvim/.config/nvim/lua/adam/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/adam/plugins/autopairs.lua
@@ -1,1 +1,23 @@
-return {}
+return {
+  "windwp/nvim-autopairs",
+  event = {"InsertEnter"},
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+  }, 
+  config = function()
+    local autopairs = require("nvim-autopairs")
+
+    autopairs.setup({
+      check_ts = true,
+      ts_config = {
+        lua = {"string"},
+      },
+    })
+
+    local cmp_autoparis = require("nvim-autopairs.completion.cmp")
+
+    local cmp = require("cmp")
+
+    cmp.event:on("confirm_done", cmp_autoparis.on_confirm_done())
+  end,
+}
